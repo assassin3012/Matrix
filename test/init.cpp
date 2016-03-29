10419627b062f39a1abbea986acb5c5291eaf81d
assassin3012 <assassin3012@mail.ru>
2016-03-30 01:56:21 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -5,7 +5,7 @@
 SCENARIO("Matrix init", "[init]") {
 	GIVEN("The number of rows and columns") {
 		auto rows = 3;
-		auto columns = 3;
+		auto columns = 4;
 		WHEN("Create instansce of Matrix") {
 			Matrix A(rows, columns);
 			Matrix B(rows*2+1,columns*2+1);
@@ -13,11 +13,21 @@ SCENARIO("Matrix init", "[init]") {
 			THEN("The number of rows and columns must be preserved") {
 				REQUIRE(A.Rows() == rows);
 				REQUIRE(A.Columns() == columns);
-				REQUIRE(B.Rows() == 2*rows+1);
-				REQUIRE(B.Columns() == 2*columns+1);
+				REQUIRE(B.Rows() == 2*rows+columns);
+				REQUIRE(B.Columns() == 2*columns+rows);
 				REQUIRE(C.Rows() == 0);
 				REQUIRE(C.Columns() == 0);
 			}
 		}
 	}
+}
+SCENARIO("Matrix Maxelement()", "[Maxelement]") {
+	Matrix A;
+	A.FillFromFile("A2x2.txt");
+	REQUIRE(A.Maxelement() == 6);
+}
+SCENARIO("Matrix Minelement()", "[Minelement]") {
+	Matrix A;
+	A.FillFromFile("A2x2.txt");
+	REQUIRE(A.Maxelement() == 1);
 }
