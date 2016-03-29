ee0cd3d996fb714c129730d25af9ef4d00dc24eb
assassin3012 <assassin3012@mail.ru>
2016-03-30 01:32:15 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -3,20 +3,20 @@
 #include <fstream>
 
 SCENARIO("Matrix init", "[init]") {
-
 	GIVEN("The number of rows and columns") {
-
 		auto rows = 3;
 		auto columns = 3;
-
 		WHEN("Create instansce of Matrix") {
-
-			Matrix matrix(rows, columns);
-
+			Matrix A(rows, columns);
+			Matrix B(rows*2+1,columns*2+1);
+			Matrix C;
 			THEN("The number of rows and columns must be preserved") {
-
-				REQUIRE(matrix.Rows() == rows);
-				REQUIRE(matrix.Columns() == columns);
+				REQUIRE(A.Rows() == rows);
+				REQUIRE(A.Columns() == columns);
+				REQUIRE(A.Rows() == 2*rows+1);
+				REQUIRE(A.Columns() == 2*columns+1);
+				REQUIRE(A.Rows() == 0);
+				REQUIRE(A.Columns() == 0);
 			}
 		}
 	}
