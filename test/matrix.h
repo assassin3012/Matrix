9077758877b323edf2238ebd7de293b475b0de23
assassin3012 <assassin3012@mail.ru>
2016-03-30 14:40:56 +0400

    Update matrix.h

--- test/matrix.h
+++ test/matrix.h
@@ -21,7 +21,7 @@ class Matrix
 {
 public:
 	Matrix<T>() :n(0), m(0), matrix(nullptr) {}
-	Matrix<T>(int rows, int columns);
+	Matrix<T>(int rows, int columns)
 	Matrix<T>(const Matrix &copy);
 	~Matrix<T>();
 	Matrix<T> operator + (const Matrix<T>&);
@@ -39,5 +39,42 @@ private:
 	int n;	// ñòðîê
 	int m;	// ñòîëáöîâ
 };
-
+template <typename T>
+Matrix<T>::Matrix(int rows, int columns) :n(rows), m(columns)
+{
+	matrix = new T*[n];
+	for (int i = 0; i<n; i++)
+	{
+		matrix[i] = new T[m];
+		for (int j = 0; j<m; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+}
+template <typename T>
+Matrix<T>::Matrix(const Matrix& copy) :n(copy.n), m(copy.m)
+{
+	matrix = new T*[n];
+	for (int i = 0; i<n; i++)
+	{
+		matrix[i] = new T[m];
+		for (int j = 0; j<m; j++)
+		{
+			matrix[i][j] = copy.matrix[i][j];
+		}
+	}
+}
+template <typename T>
+Matrix<T>::~Matrix()
+{
+	if (matrix != nullptr)
+	{
+		for (int i = 0; i<n; i++)
+		{
+			delete[] matrix[i];
+		}
+		delete[] matrix;
+	}
+}
 #endif
