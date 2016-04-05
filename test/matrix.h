75ebce10d7ce2cae9b06ae635abf1c6bce9565c4
assassin3012 <assassin3012@mail.ru>
2016-04-06 00:58:13 +0300

    Update matrix.h

--- test/matrix.h
+++ test/matrix.h
@@ -6,36 +6,7 @@ using namespace std;
 
 template <typename T>
 class Matrix;
-/*
-template <class T>
-std::ostream &operator << (std::ostream &out, const Matrix<T> &);
 
-template <class T>
-std::istream &operator >> (std::istream &input, Matrix<T> &);
-*/
-template <typename T>
-class Matrix
-{
-public:
-	Matrix<T>() :n(0), m(0), matrix(nullptr) {}
-	Matrix(int rows, int columns);
-	Matrix(const Matrix &copy);
-	~Matrix<T>();
-	Matrix operator + (const Matrix&);
-	Matrix operator - (const Matrix&);
-	Matrix operator * (const Matrix&);
-	Matrix &operator = (const Matrix&);
-	bool operator == (const Matrix&);
-	T* operator [] (int);
-	int Rows() const;
-	int Columns() const;
-	friend std::ostream &operator << <>(std::ostream &out, const Matrix<T> &temp);
-	friend std::istream &operator >> <>(std::istream &input, Matrix<T> &matr);
-private:
-	T **matrix;
-	int n;	// ñòðîê
-	int m;	// ñòîëáöîâ
-};
 template <typename T>
 std::ostream &operator << (std::ostream &out, const Matrix<T> &temp)
 {
@@ -64,5 +35,28 @@ std::istream &operator >> (std::istream &input, Matrix<T> &matr)
     }
     return input;
 }
+template <typename T>
+class Matrix
+{
+public:
+	Matrix<T>() :n(0), m(0), matrix(nullptr) {}
+	Matrix(int rows, int columns);
+	Matrix(const Matrix &copy);
+	~Matrix<T>();
+	Matrix operator + (const Matrix&);
+	Matrix operator - (const Matrix&);
+	Matrix operator * (const Matrix&);
+	Matrix &operator = (const Matrix&);
+	bool operator == (const Matrix&);
+	T* operator [] (int);
+	int Rows() const;
+	int Columns() const;
+	friend std::ostream &operator << <>(std::ostream &out, const Matrix<T> &temp);
+	friend std::istream &operator >> <>(std::istream &input, Matrix<T> &matr);
+private:
+	T **matrix;
+	int n;	// ñòðîê
+	int m;	// ñòîëáöîâ
+};
 #include "matrix.cpp"
 #endif
