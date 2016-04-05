822de64c52b0d21a91e32015d0c39680a39ed882
assassin3012 <assassin3012@mail.ru>
2016-04-06 00:33:07 +0300

    Update matrix.h

--- test/matrix.h
+++ test/matrix.h
@@ -36,5 +36,33 @@ private:
 	int n;	// ñòðîê
 	int m;	// ñòîëáöîâ
 };
+template <typename T>
+std::ostream &operator << (std::ostream &out, const Matrix<T> &temp)
+{
+	for (int i = 0; i < temp.n; i++)
+	{
+		for (int j = 0; j < temp.m; j++)
+		{
+			out << temp.matrix[i][j] << " ";
+		}
+		out << endl;
+	}
+	return out;
+}
+template <typename T>
+std::istream &operator >> (std::istream &input, Matrix<T> &matr)
+{
+    for (int i = 0; i < matr.n; i++) 
+    {
+        for (int j = 0; j < matr.m; j++) 
+        {
+            if (!(input >> matr.matrix[i][j]))
+            {
+                throw "Error matrix fill";
+            }
+        }
+    }
+    return input;
+}
 #include "matrix.cpp"
 #endif
