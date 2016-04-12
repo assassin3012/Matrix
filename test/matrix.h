af159d40d99def6bba49cc5bb3427088e59af014
assassin3012 <assassin3012@mail.ru>
2016-04-13 01:02:37 +0300

    Update matrix.h

--- test/matrix.h
+++ test/matrix.h
@@ -52,41 +52,40 @@ public:
 	T* operator [] (unsigned int);
 	unsigned int Rows() const;
 	unsigned int Columns() const;
-	friend std::ostream &operator << <>(std::ostream &out, const Matrix<T> &temp)
-	{
-		for (int i = 0; i < temp.n; i++)
-		{
-			for (int j = 0; j < temp.m; j++)
-			{
-				out << temp.matrix[i][j] << " ";
-			}
-			out << endl;
-		}
-		return out;
-	}
-	friend std::istream &operator >> <>(std::istream &input, Matrix<T> &matr)
-	{
-		for (int i = 0; i < matr.n; i++) 
-		{
-        		for (int j = 0; j < matr.m; j++) 
-        		{
-        			try {
-            				if (!(input >> matr.matrix[i][j]))
-            				{
-                				throw FillException();
-            				}
-        			}
-        			catch (...) {
-        				throw FillException();
-        			}
-        		}
-	 	}
-	 	return input;
-	};
+	friend std::ostream &operator << <>(std::ostream &out, const Matrix<T> &temp);
+	friend std::istream &operator >> <>(std::istream &input, Matrix<T> &matr);
 private:
 	T **matrix;
 	unsigned int n;	// ñòðîê
 	unsigned int m;	// ñòîëáöîâ
 };
+template <typename T>
+std::ostream &operator << (std::ostream &out, const Matrix<T> &temp);
+{
+	for (int i = 0; i < temp.n; i++)
+	{
+		for (int j = 0; j < temp.m; j++)
+		{
+			out << temp.matrix[i][j] << " ";
+		}
+		out << endl;
+	}
+	return out;
+}
+template <typename T>
+std::istream &operator >> (std::istream &input, Matrix<T> &matr);
+{
+    for (int i = 0; i < matr.n; i++) 
+    {
+        for (int j = 0; j < matr.m; j++) 
+        {
+            if (!(input >> matr.matrix[i][j]))
+            {
+                throw "Error matrix fill";
+            }
+        }
+    }
+    return input;
+}
 #include "matrix.cpp"
 #endif
