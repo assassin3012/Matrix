7a3ff2bcff3a0bfec4c570df3b205ab683906b28
assassin3012 <assassin3012@mail.ru>
2016-04-13 00:53:08 +0300

    Update matrixexception.h

--- test/matrixexception.h
+++ test/matrixexception.h
@@ -6,7 +6,7 @@ public:
   MatrixException(const char* msg): msg(msg) {};
 private:
   char* msg;
-}
+};
 
 class SizeException: public MatrixException 
 {
@@ -14,25 +14,25 @@ public:
   SizeException(): MatrixException("Sizes don't match") {};
 private:
   char* msg;
-}
+};
 class EmptyException: public MatrixException 
 {
 public:
   EmptyException(): MatrixException("Matrix empty") {};
 private:
   char* msg;
-}
+};
 class FillException: public MatrixException 
 {
 public:
   FillException(): MatrixException("Matrix fill") {};
 private:
   char* msg;
-}
+};
 class RowException: public MatrixException 
 {
 public:
   RowException(): MatrixException("Row's index error") {};
 private:
   char* msg;
-}
+};
