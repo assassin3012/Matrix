8ae4366677d5ac5e0a58559bde4c69c40d855303
assassin3012 <assassin3012@mail.ru>
2016-04-17 23:59:15 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -269,7 +269,7 @@ SCENARIO("Template test", "[Ttest]")
 	Matrix<char> resultchar(2, 2);
 	std::ifstream("A.txt") >> Achar;
 	std::ifstream("B.txt") >> Bchar;
-	std::ifstream("AxB.txt") >> resultchar;
+	std::ifstream("AxBchar.txt") >> resultchar;
 	expectedchar=Achar*Bchar;
 	REQUIRE(expectedchar==resultchar);
 }
