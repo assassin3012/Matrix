c38b3d15123af76f73a8f012a66d4866f5cea9e6
assassin3012 <assassin3012@mail.ru>
2016-09-12 09:33:36 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -7,12 +7,12 @@
 SCENARIO("Stack", "[all]") 
 {
 	stack <bool> b;
-	REQUIRE( b.count() == 0);
+	REQUIRE( b.count() == 0)
 	stack <int> a;
 	a.push(1);
 	a.push(2);
 	a.push(3);
-	REQUIRE( a.pop() == 3);
+	REQUIRE( a.pop() == 3)
 	REQUIRE( a.count() == 2)
-	REQUIRE( a.pop() == 2);
+	REQUIRE( a.pop() == 2)
 }
